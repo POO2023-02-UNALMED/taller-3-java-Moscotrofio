7f856f7227536adc072b9f17218100802c1b6264
Moscotrofio <indarttomas@gmail.com>
2023-08-28 12:20:34 -0500

    Update Control.java

--- src/main/java/taller3/televisores/Control.java
+++ src/main/java/taller3/televisores/Control.java
@@ -1,65 +1,65 @@
 package taller3.televisores;
 
 public class Control {
-    private TV tv;
+	
+	private TV tv;
+	
+	public void turnOn() {
+		
+		this.tv.turnOn();
+	}
+	
+	public void turnOff() {
+		
+		this.tv.turnOff();
+	}
+	
+	public void canalUp() {
+		
+		tv.canalUp();
+	}
+	
+	public void canalDown() {
+		
+		tv.canalDown();
+	}
+	
+	public void volumenUp() {
+		
+		tv.volumenUp();
+	}
+	
+	public void volumenDown() {
+		
+		tv.volumenDown();
+	}
+	
+	public void setCanal(int canal) {
+		
+		tv.setCanal(canal);
+	}
+	
+	public void setVolumen(int volumen) {
+		
+		tv.setVolumen(volumen);
+	}
+	
+	public void enlazar(TV televisor) {
+		
+		this.tv = televisor;
+		
+		this.tv.setControl(this);
+		
+	}
+	
+	public TV getTv() {
+		
+		return tv;
+	}
+	
+	public void setTv(TV obj) {
+		
+		tv = obj;
+	}
 
-    public void enlazar(TV tv) {
-        this.setTv(tv);
-        this.tv.setControl(this);
-    }
-
-    public TV getTv() {
-        return tv;
-    }
-    public void setTv(TV tv) {
-        this.tv = tv;
-    }
-
-    public void turnOn(){
-        if (tv != null) {
-            tv.turnOn();
-        }
-    }
-    public void turnOff() {
-        if (tv != null) {
-            tv.turnOff();
-        }
-    }
-
-    public void canalUp() {
-        if (tv != null) {
-            tv.canalUp();
-        }
-    }
-
-    public void canalDown() {
-        if (tv != null) {
-            tv.canalDown();
-        }
-    }
-
-    public void volumenUp() {
-        if (tv != null) {
-            tv.volumenUp();
-        }
-    }
-
-    public void volumenDown() {
-        if (tv != null) {
-            tv.volumenDown();
-        }
-    }
-    
-
-    public void setCanal(int canal) {
-        if (tv != null) {
-            tv.setCanal(canal);
-        }
-    }
-
-    public void setVolumen(int volumen) {
-        if (tv != null) {
-            tv.setVolumen(volumen);
-        }
-    }
 }
