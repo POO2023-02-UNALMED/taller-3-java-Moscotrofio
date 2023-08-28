70ae886119f791c955c7f0a67e3435cd243080f8
Moscotrofio <indarttomas@gmail.com>
2023-08-28 11:45:17 -0500

    Update Control.java

--- src/main/java/taller3/televisores/Control.java
+++ src/main/java/taller3/televisores/Control.java
@@ -1,70 +1,70 @@
 package taller3.televisores;
 
 public class Control {
-    TV tv;
+    private TV tv;
 
     public void enlazar(TV tv) {
         tv.setControl(this);
         this.setTv(tv);
     }
 
-    public TV getTv() {
-        return this.tv;
+    public static TV getTv() {
+        return tv;
     }
-    public void setTv(TV tv) {
+    public static void setTv(TV tv) {
         this.tv = tv;
     }
 
     public void turnOn(){
-        if (this != null) {
+        if (tv != null) {
             this.tv.estado = true;
         }
     }
     public void turnOff() {
-        if (this.tv != null) {
-            this.tv.estado = false;
+        if (tv != null) {
+            tv.estado = false;
         }
     }
 
     public void canalUp() {
-        if (this != null) {
-            this.tv.canalUp();
+        if (tv != null) {
+            tv.canalUp();
         }
     }
 
     public void canalDown() {
-        if (this != null) {
-            this.tv.canalDown();
+        if (tv != null) {
+            tv.canalDown();
         }
     }
 
     public void volumenUp() {
-        if (this != null) {
-            this.tv.volumenUp();
+        if (tv != null) {
+            tv.volumenUp();
         }
     }
 
     public void volumenDown() {
-        if (this != null) {
-            this.tv.volumenDown();
+        if (tv != null) {
+            tv.volumenDown();
         }
     }
     
 
     public void setCanal(int canal) {
-        if (this != null) {
-            this.tv.setCanal(canal);
+        if (tv != null) {
+            tv.setCanal(canal);
         }
     }
 
     public void setVolumen(int volumen) {
-        if (this != null) {
-            this.tv.setVolumen(volumen);
+        if (tv != null) {
+            tv.setVolumen(volumen);
         }
     }
     
     public boolean getEstado() {
-        return this.tv.estado;
+        return tv.estado;
     }
 
 }
